23bd618d04e9b947c319ca9685cb2be13030d65c
Sankara <chimpy.the.dev@outlook.com>
2020-11-10 16:51:16 +0300

    move python files

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
